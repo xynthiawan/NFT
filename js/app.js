2046f0db0ec56040bbac5e609f3a47ba3a7d181e
xynthiawan <xdw201205@gmail.com>
2023-02-22 13:42:29 -0500

    so much shit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+import Moralis  from 'moralis';
+import { EvmChain } from '@moralisweb3/evm-utils';
+
+try {
+    const address = '0x1234567890123456789012345678901234567890';
+
+    const chain = EvmChain.ETHEREUM;
+
+    await Moralis.start({
+        apiKey: 'HPv3BCMpE4QCggWHASRGICvAN3uyXZgngh5EBDcfIjqR89rMzMlOZ3vWW5RrQh9i',
+        // ...and any other configuration
+    });
+
+    const response = await Moralis.EvmApi.nft.getWalletNFTs({
+        address,
+        chain,
+    });
+
+    console.log(response?.result);
+} catch (e) {
+    console.error(e);
+}
